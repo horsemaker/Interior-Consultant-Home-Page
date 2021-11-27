04d336d301a0288ccb328a7e20d6fc9796e544d1
horsemaker <yash_ghodekar@yahoo.in>
2021-11-27 12:43:37 +0530

    tried with resize & innerwidth

--- script.js
+++ script.js
@@ -20,14 +20,53 @@ function menuAction(event) {
 }
 
 menuBtn.addEventListener('click', menuAction)
+
+// var previousWindowSize = undefined
+// var currentWindowSize = window.innerWidth
+
 window.addEventListener('resize', () => {
   // console.log("resized!")
   // console.log(window.innerWidth)
+
   if (window.innerWidth > 480) {
     menu.style.visibility = 'visible'
   } else {
+    // if (menu.style.visibility === 'visible') {
+    //   return
+    // }
     menu.style.visibility = 'hidden'
     openMenu.style.display = 'block'
     closeMenu.style.display = 'none'
   }
+  // console.log("Previous Window Size", previousWindowSize)
+  // console.log("Current Window Size", currentWindowSize)
+
+  // if (previousWindowSize === undefined) {
+  //   if (currentWindowSize > 480) {
+  //     menu.style.visibility = 'visible'
+  //   } else {
+  //     menu.style.visibility = 'hidden'
+  //     openMenu.style.display = 'block'
+  //     close.style.display = 'none'
+  //   }
+  // } else if (previousWindowSize > 480) {
+  //   if (currentWindowSize < 480) {
+  //     menu.style.visibility = 'hidden'
+  //     openMenu.style.display = 'block'
+  //     close.style.display = 'none'
+  //   } else {
+  //     menu.style.visibility = 'visible'
+  //   }
+  // } else {
+  //   // previousWindowSize < 480
+  //   if (currentWindowSize < 480) {
+  //     if (menu.style.visibility === 'visible') {
+  //     }
+  //   } else {
+  //     // currentWindowSize > 480
+  //     menu.style.visibility = 'visible'
+  //   }
+  // }
+
+  // previousWindowSize = currentWindowSize
 })
