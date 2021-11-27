835b5542016420de083f664a86b20b0b13b98295
horsemaker <yash_ghodekar@yahoo.in>
2021-11-27 13:59:59 +0530

    fix: is this the final fix?

--- script.js
+++ script.js
@@ -1,72 +1,42 @@
-var menuBtn = document.querySelector('.page__menu')
+// var menuBtn = document.querySelector('.page__menu')
+// var openMenu = document.querySelector('#open-menu')
+// var closeMenu = document.querySelector('#close-menu')
+// var menu = document.querySelector('.page__navlinks')
+
+// function menuAction(event) {
+//   // console.log('clicked!')
+
+//   if (event.target.id === 'open-menu') {
+//     // console.log("open menu")
+//     openMenu.style.display = 'none'
+//     closeMenu.style.display = 'block'
+//     menu.style.visibility = 'visible'
+//   } else {
+//     // console.log("close menu")
+//     openMenu.style.display = 'block'
+//     closeMenu.style.display = 'none'
+//     menu.style.visibility = 'hidden'
+//   }
+// }
+
+// menuBtn.addEventListener('click', menuAction)
+
+var hamburger = document.querySelector('#page__menu')
 var openMenu = document.querySelector('#open-menu')
 var closeMenu = document.querySelector('#close-menu')
-var menu = document.querySelector('.page__navlinks')
+var navLinks = document.querySelector('#page__navlinks')
+var body = document.querySelector('body')
 
-function menuAction(event) {
-  // console.log('clicked!')
-
+hamburger.addEventListener('click', (event) => {
   if (event.target.id === 'open-menu') {
-    // console.log("open menu")
     openMenu.style.display = 'none'
     closeMenu.style.display = 'block'
-    menu.style.visibility = 'visible'
+    body.classList.toggle('body__height')
+    navLinks.classList.toggle('show')
   } else {
-    // console.log("close menu")
     openMenu.style.display = 'block'
     closeMenu.style.display = 'none'
-    menu.style.visibility = 'hidden'
+    body.classList.toggle('body__height')
+    navLinks.classList.toggle('show')
   }
-}
-
-menuBtn.addEventListener('click', menuAction)
-
-// var previousWindowSize = undefined
-// var currentWindowSize = window.innerWidth
-
-// window.addEventListener('resize', () => {
-// console.log("resized!")
-// console.log(window.innerWidth)
-
-// if (window.innerWidth > 480) {
-//   menu.style.visibility = 'visible'
-// } else {
-//   // if (menu.style.visibility === 'visible') {
-//   //   return
-//   // }
-//   menu.style.visibility = 'hidden'
-//   openMenu.style.display = 'block'
-//   closeMenu.style.display = 'none'
-// }
-// // console.log("Previous Window Size", previousWindowSize)
-// console.log("Current Window Size", currentWindowSize)
-
-// if (previousWindowSize === undefined) {
-//   if (currentWindowSize > 480) {
-//     menu.style.visibility = 'visible'
-//   } else {
-//     menu.style.visibility = 'hidden'
-//     openMenu.style.display = 'block'
-//     close.style.display = 'none'
-//   }
-// } else if (previousWindowSize > 480) {
-//   if (currentWindowSize < 480) {
-//     menu.style.visibility = 'hidden'
-//     openMenu.style.display = 'block'
-//     close.style.display = 'none'
-//   } else {
-//     menu.style.visibility = 'visible'
-//   }
-// } else {
-//   // previousWindowSize < 480
-//   if (currentWindowSize < 480) {
-//     if (menu.style.visibility === 'visible') {
-//     }
-//   } else {
-//     // currentWindowSize > 480
-//     menu.style.visibility = 'visible'
-//   }
-// }
-
-// previousWindowSize = currentWindowSize
-// })
+})
